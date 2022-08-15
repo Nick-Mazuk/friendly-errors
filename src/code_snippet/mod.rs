59b1d1e8fcb24992582451df550dc964cf2c79b8
Nick Mazuk <Nick-Mazuk@users.noreply.github.com>
2022-08-14 22:40:20 -0700

    add caption and file url to code snippets

--- src/code_snippet/mod.rs
+++ src/code_snippet/mod.rs
@@ -18,7 +18,7 @@ type CalculatedFieldResult<T> = Result<T, CalculatedFieldError>;
 #[derive(PartialEq, Debug, Clone)]
 pub struct FriendlyCodeSnippet {
     file_contents: String,
-    file_path: String,
+    file_path: Option<String>,
     index_start: Option<usize>,
     index_end: Option<usize>,
     line_start: Option<usize>,
@@ -49,11 +49,25 @@ fn get_digit_count(mut number: usize) -> usize {
     digits
 }
 
+fn get_line_number_string(line_number: usize, indent: usize) -> String {
+    let number = line_number.to_string();
+    let mut output = " ".repeat(indent - 1 - number.len());
+    output.push_str(&number);
+    output.push_str(" | ");
+    output
+}
+
+fn get_blank_line_prefix(indent: usize) -> String {
+    let mut output = " ".repeat(indent);
+    output.push_str("| ");
+    output
+}
+
 impl FriendlyCodeSnippet {
     pub fn new(file_contents: String) -> Self {
         FriendlyCodeSnippet {
             file_contents,
-            file_path: String::new(),
+            file_path: None,
             index_start: None,
             index_end: None,
             line_start: None,
@@ -69,7 +83,7 @@ impl FriendlyCodeSnippet {
     }
 
     pub fn set_file_path<S: Into<String>>(mut self, file_path: S) -> Self {
-        self.file_path = file_path.into();
+        self.file_path = Some(file_path.into());
         self
     }
 
@@ -101,14 +115,6 @@ impl FriendlyCodeSnippet {
     pub fn caption<S: Into<String>>(mut self, caption: S) -> Self {
         self.caption = Some(caption.into());
         self
-    }
-
-    pub(crate) fn build(mut self) -> Result<String, FriendlyCodeSnippetError> {
-        self.calc_line_start_start_index();
-        self.calc_line_end_start_index();
-        self.validate_inputs()?;
-        self.calc_indent_size();
-        Ok(String::new())
     }
 
     pub(crate) fn calc_line_start_start_index(&mut self) {
@@ -194,6 +200,62 @@ impl FriendlyCodeSnippet {
             get_digit_count(longest_line_number) + 1,
             default_indent_size,
         ));
+    }
+
+    pub(crate) fn build_file_url(&self) -> String {
+        let mut output = " ".repeat(self.indent_size.unwrap());
+        let mut has_contents = false;
+        if let Some(file_path) = &self.file_path {
+            output.push_str(file_path);
+            has_contents = true;
+        }
+        if let Some(line_start) = self.line_start {
+            if has_contents {
+                output.push(':');
+            }
+            output.push_str(&line_start.to_string());
+            has_contents = true;
+        }
+        if let Some(index_start) = self.index_start {
+            if has_contents {
+                output.push(':');
+            }
+            output.push_str(&index_start.to_string());
+            has_contents = true;
+        }
+        if !has_contents {
+            return String::new();
+        }
+        output.push('\n');
+        output
+    }
+
+    pub(crate) fn build_caption(&self) -> String {
+        if let Some(caption) = &self.caption {
+            let mut output = " ".repeat(self.indent_size.unwrap() - 2);
+            output.push_str("--> ");
+            output.push_str(caption);
+            output.push('\n');
+            return output;
+        }
+        String::new()
+    }
+
+    #[cfg(test)]
+    pub(crate) fn set_indent_size(mut self, indent_size: usize) -> Self {
+        self.indent_size = Ok(indent_size);
+        self
+    }
+
+    pub(crate) fn build(mut self) -> Result<String, FriendlyCodeSnippetError> {
+        self.calc_line_start_start_index();
+        self.calc_line_end_start_index();
+        self.validate_inputs()?;
+        self.calc_indent_size();
+        let mut output = String::new();
+        output.push_str(&self.build_file_url());
+        output.push_str(&self.build_caption());
+        Ok(output)
     }
 }
 
@@ -339,5 +401,105 @@ mod test {
         assert_eq!(get_digit_count(101), 3);
         assert_eq!(get_digit_count(999), 3);
         assert_eq!(get_digit_count(1001), 4);
+    }
+
+    #[test]
+    fn get_line_number_string_test() {
+        assert_eq!(get_line_number_string(1, 4), "  1 | ");
+        assert_eq!(get_line_number_string(2, 4), "  2 | ");
+        assert_eq!(get_line_number_string(20, 4), " 20 | ");
+        assert_eq!(get_line_number_string(200, 4), "200 | ");
+        assert_eq!(get_line_number_string(200, 5), " 200 | ");
+    }
+
+    #[test]
+    fn get_blank_line_prefix_test() {
+        assert_eq!(get_blank_line_prefix(4), "    | ");
+        assert_eq!(get_blank_line_prefix(5), "     | ");
+        assert_eq!(
+            get_blank_line_prefix(7).len(),
+            get_line_number_string(1, 7).len()
+        );
+    }
+
+    #[test]
+    fn build_file_url_test() {
+        assert_eq!(
+            FriendlyCodeSnippet::new(String::new())
+                .set_indent_size(4)
+                .build_file_url(),
+            ""
+        );
+        assert_eq!(
+            FriendlyCodeSnippet::new(String::new())
+                .index_start(4)
+                .set_indent_size(4)
+                .build_file_url(),
+            "    4\n"
+        );
+        assert_eq!(
+            FriendlyCodeSnippet::new(String::new())
+                .line_start(24)
+                .index_start(4)
+                .set_indent_size(4)
+                .build_file_url(),
+            "    24:4\n"
+        );
+        assert_eq!(
+            FriendlyCodeSnippet::new(String::new())
+                .line_start(24)
+                .set_indent_size(4)
+                .build_file_url(),
+            "    24\n"
+        );
+        assert_eq!(
+            FriendlyCodeSnippet::new(String::new())
+                .set_file_path("hello.rs")
+                .set_indent_size(4)
+                .build_file_url(),
+            "    hello.rs\n"
+        );
+        assert_eq!(
+            FriendlyCodeSnippet::new(String::new())
+                .set_file_path("hello.rs")
+                .line_start(24)
+                .index_start(4)
+                .set_indent_size(4)
+                .build_file_url(),
+            "    hello.rs:24:4\n"
+        );
+        assert_eq!(
+            FriendlyCodeSnippet::new(String::new())
+                .set_file_path("hello.rs")
+                .line_start(24)
+                .index_start(4)
+                .set_indent_size(8)
+                .build_file_url(),
+            "        hello.rs:24:4\n"
+        );
+    }
+
+    #[test]
+    fn build_caption_test() {
+        assert_eq!(
+            FriendlyCodeSnippet::new(String::new())
+                .set_indent_size(4)
+                .build_caption(),
+            ""
+        );
+        assert_eq!(
+            FriendlyCodeSnippet::new(String::new())
+                .caption("hello world")
+                .set_indent_size(4)
+                .build_caption(),
+            "  --> hello world\n"
+        );
+        assert_eq!(
+            FriendlyCodeSnippet::new(String::new())
+                .caption("hello world")
+                .set_indent_size(8)
+                .build_caption(),
+            "      --> hello world\n"
+        );
     }
 }
